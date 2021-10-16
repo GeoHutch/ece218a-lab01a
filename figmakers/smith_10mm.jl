b205f67b62e754497bbec6d08151d40af6357b4f
George Higgins Hutchinson <george@hutch.space>
2021-10-15 23:07:11 -0700

    adds new through-line smith chart figures

--- figmakers/smith_10mm.jl
+++ figmakers/smith_10mm.jl
@@ -4,7 +4,7 @@ using LaTeXStrings
 
 include("../ads_text_reader.jl")
 
-data = read_two_port("text_data/5mm.txt")
+data = read_two_port("text_data/10mm.txt")
 
 Z0 = 50
 
@@ -17,24 +17,25 @@ function first_at_least(xs,n)
 end
 
 Z(Gamma) = (1 + Gamma)/(1 - Gamma)
+Gamma(Z) = (Z-1)/(Z+1)
 
 ws_meas = data.fs .* 2pi
 S11_meas = data.S11
 S11_interp = resample(S11_meas, 4)
 
-S11_theory(w) = 1-(1-0.865)/2 + (1-0.865)/2*exp(im*w)
+S11_theory(w) = 1-(1-0.310)/2 + (1-0.865)/2*exp(im*w)
 S11 = S11_theory.(ws_meas)
 
-r_crossing_idx = first_at_least(findall(x->x>0.5, diff(sign.(imag.(S11_interp)))),10)
+r_crossing_idx = findall(x->x>0.5, diff(sign.(imag.(S11_interp))))[end]
 markpoint = S11_interp[r_crossing_idx]
 Zcross = Z(markpoint)
 
 fig = SmithAxis([
-                 PGFPlots.SmithData(Z.(S11_interp), style="blue", mark="none", legendentry="5mm Wire (Measured)"),
+                 PGFPlots.SmithData(Z.(S11_interp), style="blue", mark="none", legendentry="10mm Wire (Measured)"),
                  PGFPlots.SmithData([Zcross], style="cyan", mark="square", legendentry="Z=$(Z0*real(Zcross))"),
-                 PGFPlots.SmithCircle(1-(1-0.865)/2,0.0,(1-0.865)/2; style="red", texlabel="5mm Wire (Theory)")
+                 PGFPlots.SmithCircle(1-(1-0.310)/2-0.05,0.0,1-0.310; style="red", texlabel="10mm Wire (Theory)")
                  #  PGFPlots.SmithData(Z.(S11), style="red", mark="none", legendentry="5mm Wire (Theory)"),
 ])
 
-save("figures/build/smith_5mm.pdf",fig)
+save("figures/build/smith_10mm.pdf",fig)
 
